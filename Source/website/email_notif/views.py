24d0deca2b2a2c82f41359278551aee50a95da2b
SolsticeDante <solsticedante@gmail.com>
2019-04-21 16:19:15 +0800

    haldway through acnapi_email() function

--- Source/website/email_notif/views.py
+++ Source/website/email_notif/views.py
@@ -3,7 +3,9 @@ from django.core.mail import send_mail
 from django.core.mail import mail_admins
 from django.conf import settings
 from createuser.models import Extended_User
+import requests  # to access acnapi
 import smtplib
+import copy
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -64,9 +66,36 @@ def email_to_user(request):
 
 
 
-def acnapi_email():
-	"""
-	Private method, sends email to destination through support@accenture.com
-	"""
-	payload = {"subject":None, "sender":None, "recipient":None, "html":None}
-	header = {"Host":None, "Server-Token":None, "Content-Type":None, "cache-control":None, "Postman-Token":None}
+def acnapi_email(email_subject, email_recipient, email_html):
+        """
+        Private method, sends email to destination through support@accenture.com
+        """
+        url = "https://ug-api.acnapiv3.io/swivel/email-services/api/mailer"
+        payload = {"subject":None, "sender":"support@accenture.com", "recipient":None, "html":None}
+        headers = {"Host":"ug-api.acnapiv3.io", "Server-Token":None, "Content-Type":"application/json", "cache-control":"no-cache",  $
+        token_file_contents = None
+        counter0 = 2  # a counter that counts the number of values we need to retrieve from tokens.txt
+
+        with open("tokens.txt") as token_file:
+                token_file_contents = copy.deepcopy(token_file.readlines())
+
+        for i in token_file_contents:
+                if "Server-Token" in i:
+                        headers["Server-Token"] = i.split(":")[1][:-1]  # slice the string to remove "/n" char
+                        counter0 -= 1
+                elif "Postman-Token" in i:
+                        headers["Postman-Token"] = i.split(":")[1][:-1]  # slice the string to remove "/n char"
+                        counter0 -= 1
+
+        # check if all necessary values are retrieved
+        if counter0 != 0:
+                print("tokens.txt do not have the values acnapi_email() is looking for")
+                return
+
+        payload["subject"] = email_subject
+        payload["recipient"] = email_recipient
+        payload["html"] = email_html
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.status_code==200)
+
